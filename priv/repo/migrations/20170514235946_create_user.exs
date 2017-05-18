460db8ac0f22be567845e1e291ca676e0e827766
Antonis Berkakis <aberkakis@gmail.com>
2017-05-18 23:02:06 +0100

    setting up migration

--- priv/repo/migrations/20170514235946_create_user.exs
+++ priv/repo/migrations/20170514235946_create_user.exs
@@ -13,3 +13,8 @@ defmodule Rumbl.Repo.Migrations.CreateUser do
     create unique_index(:users, [:username])
   end
 end
+
+Repo.insert(%User{name: "Jos", username: "josevalim", password_hash: "password" })
+
+Repo.insert(%User{name: "Bruce", username: "redrapids", password_hash: "password" })
+Repo.insert(%User{name: "Chris", username: "cmccord", password_hash: "password" })
