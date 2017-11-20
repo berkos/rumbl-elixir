9c3965bab58ac29f029bce9d116711b9d0a7dae3
Antonis Berkakis <aberkakis@gmail.com>
2017-11-20 23:43:47 +0000

    wip

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,14 +6,35 @@ defmodule Rumbl.User do
    field :username, :string
    field :password, :string, virtual: true
    field :password_hash, :string
- 	
+
    timestamps()
   end
-  
+
   def changeset(model, params \\ :empty) do
     model
+    |> changeset(params)
     # make sure it checks the params with cast
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, pass)
+      _ ->
+        changeset
+    end
+  end
+
+
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
   end
 end
